6c86ba94a3d4eb247d0878cf202c5a0abfa86bc8
MAJID-MSH <91826411+MAJID-MSH@users.noreply.github.com>
2021-10-03 13:50:34 +0330

    Add files via upload

---
+++ RadiusCircle.py
@@ -0,0 +1,11 @@
+print('Hi')
+print('Enter Radius Of Circle !!:')
+R = float(input())
+
+P = (2*R*3.14)
+A = (R*R*3.14)
+
+print('Perimeter  of circle is :', P)
+print('Area Of Circle is :' , A)
+
+print('\n   ----MADE BY MAJID-MSH :)----\n\t<----GOODLUCK---->\n--------------------------------------------')
