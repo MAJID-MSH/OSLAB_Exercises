778a41a99250b75cd8a034e56c523990491c5342
MAJID-MSH <91826411+MAJID-MSH@users.noreply.github.com>
2021-10-12 23:40:37 +0330

    Add files via upload

---
+++ Second_Exercise/Second to Time.py
@@ -0,0 +1,11 @@
+print('Hi')
+
+Second = int(input('Enter Second: '))
+Hour=int (Second/3600)
+Second=Second%3600
+Minute=int (Second/60)
+Second=Second%60
+
+print('\nOUTPUT =>',  Hour ,":", Minute ,":", Second )
+
+print('\n   ---- MADE BY MAJID-MSH :)----\n\t<----GOODLUCK---->\n--------------------------------------------')
