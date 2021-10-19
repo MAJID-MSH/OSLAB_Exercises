e46ecebea2ee3c86234ac59def81bf6231974d1e
MAJID-MSH <91826411+MAJID-MSH@users.noreply.github.com>
2021-10-20 02:12:51 +0330

    Add files via upload

---
+++ Third_Exercise/3-Word-Counter.py
@@ -0,0 +1,12 @@
+print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+print('Hi')
+
+Arr = []
+print("Please Enter Your Sentence :")
+Arr = input()
+
+temp = Arr.split(" ")
+L = len(temp)
+
+print("\n--> Number of Word:" , L , "\nList of word" , temp )
+print('\n   ---- MADE BY MAJID-MSH :)----\n\t<----GOODLUCK---->\n--------------------------------------------')
