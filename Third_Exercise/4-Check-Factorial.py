5d246a96b8699a5d24f43bfc0c56c5d2e1a9d19b
MAJID-MSH <91826411+MAJID-MSH@users.noreply.github.com>
2021-10-20 03:16:23 +0330

    Add files via upload

---
+++ Third_Exercise/4-Check-Factorial.py
@@ -0,0 +1,21 @@
+print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+print('Hi')
+
+Num = int (input("Please Enter Your Number :"))
+f = 1
+i = 1 
+
+while True :
+    f = f*i
+    i +=1
+    
+
+    if f == Num  :
+        print("YES!")
+        break
+    
+    elif f > Num :
+        print("NO!")
+        break
+        
+print('\n   ---- MADE BY MAJID-MSH :)----\n\t<----GOODLUCK---->\n--------------------------------------------')
